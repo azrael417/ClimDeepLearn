626e2032199f3b3749cb33a392589c7d5b7a4406
Mayur Mudigonda <mudigonda@berkeley.edu>
2018-01-31 01:06:14 -0800

    Updated the graph to match the keras onem ore accurately with wd and dropout

--- semanticsegm/tiramisu-tf/tiramisu-tf.py
+++ semanticsegm/tiramisu-tf/tiramisu-tf.py
@@ -9,7 +9,8 @@ image_width = 144
 def conv(x, nf, sz, wd, stride=1): 
     return tf.layers.conv2d(x, nf, sz, strides=(stride,stride), padding='same',
                             #kernel_initializer='he_uniform',
-                            #bias_initializer=tf.zeros_zeros_initializer(),
+                            kernel_initializer= tf.initializers.random_uniform(),
+                            bias_initializer=tf.initializers.random_uniform(),
                             kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=wd)
                             )
 
@@ -21,7 +22,7 @@ def dense_block(n, x, growth_rate, p, wd, training):
             b = tf.layers.batch_normalization(x, training=training)
             b = tf.nn.relu(b)
             b = conv(b, growth_rate, sz=3, wd=wd)
-            if p: b = tf.layers.dropout(b, p=p, training=training)
+            if p: b = tf.layers.dropout(b, rate=p, training=training)
 
             x = tf.concat([x, b], axis=-1)
             added.append(b)
@@ -33,7 +34,7 @@ def transition_dn(x, p, wd, training):
         b = tf.layers.batch_normalization(x, training=training)
         b = tf.nn.relu(b)
         b = conv(b, x.get_shape().as_list()[-1], sz=1, wd=wd, stride=2)
-        if p: b = tf.layers.dropout(b, p=p, training=training)
+        if p: b = tf.layers.dropout(b, rate=p, training=training)
     return b
 
 def down_path(x, nb_layers, growth_rate, p, wd, training):
@@ -56,7 +57,11 @@ def reverse(a):
 def transition_up(added,wd,training):
     x = tf.concat(added,axis=-1) 
     _, r, c, ch = x.get_shape().as_list()
-    x = tf.layers.conv2d_transpose(inputs=x,strides=(2,2),kernel_size=(3,3),padding='same',filters=ch)
+    #x = tf.layers.conv2d_transpose(inputs=x,strides=(2,2),kernel_size=(3,3),padding='same',filters=ch,kernel_initializer='he_uniform')
+    x = tf.layers.conv2d_transpose(inputs=x,strides=(2,2),kernel_size=(3,3),
+				   padding='same',filters=ch,
+				   kernel_initializer=tf.initializers.random_uniform(),
+				   bias_initializer=tf.initializers.random_uniform())
     return x 
     
 	
@@ -67,10 +72,10 @@ def up_path(added,skips,nb_layers,growth_rate,p,wd,training):
 	x, added = dense_block(n,x,growth_rate,p,wd,training=training)
     return x
 
-# def create_tiramisu(nb_classes, img_input, nb_dense_block=6, 
-        # growth_rate=16, nb_filter=48, nb_layers_per_block=5, p=None, wd=0., training=True):
-def create_tiramisu(nb_classes, img_input, nb_dense_block=3, 
-        growth_rate=2, nb_filter=4, nb_layers_per_block=5, p=None, wd=0., training=True):
+def create_tiramisu(nb_classes, img_input, nb_dense_block=6, 
+         growth_rate=16, nb_filter=48, nb_layers_per_block=5, p=None, wd=0., training=True):
+#def create_tiramisu(nb_classes, img_input, nb_dense_block=3, 
+        #growth_rate=2, nb_filter=4, nb_layers_per_block=5, p=None, wd=0., training=True):
     
     if type(nb_layers_per_block) is list or type(nb_layers_per_block) is tuple:
         nb_layers = list(nb_layers_per_block)
@@ -144,6 +149,7 @@ def main():
     #trn = np.random.randint(255,size=(10,96,144,1))
     #trn_labels = np.random.randint(3,size=(10,96,144,1))
     #trn = (trn - trn.mean(axis=0))/trn.std(axis=0)
+    blocks = [3,3,4,7,10]
     
 
     with training_graph.as_default():
@@ -151,7 +157,7 @@ def main():
         with tf.Session() as sess:
             images = tf.placeholder(tf.float32, [None, trn.shape[1], trn.shape[2], 1])
 	    labels = tf.placeholder(tf.int32, [None, trn.shape[1], trn.shape[2], 1])
-            model = create_tiramisu(3, images)
+            model = create_tiramisu(3, images, nb_layers_per_block=blocks, p=0.2,wd=1e-4)
 	    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels,logits=model)
 	    train_op = tf.train.RMSPropOptimizer(learning_rate=1e-3).minimize(loss)
 	    tf.global_variables_initializer().run()
@@ -159,7 +165,7 @@ def main():
 	    #batch_trn = trn[indices, ...].reshape([batch,96,144,1])
 	    #batch_trn_labels = trn_labels[:batch,...].reshape([batch,96,144,1])
 	    #batch_trn_labels = trn_labels[indices, ...].reshape([batch,96,144,1])
-	    for ii in range(100):
+	    for ii in range(60):
 		l = []
 		for jj in np.arange(0,trn.shape[0],batch):
 		    try:
@@ -175,7 +181,7 @@ def main():
 		    feed_dict = {images:batch_trn,labels:batch_trn_labels}
 	            _,loss_itr = sess.run([train_op,loss],feed_dict=feed_dict)
 		    l.append(loss_itr)
-		    if np.mod(jj,640):
+		    if np.mod(jj,1280):
 			    print("Loss for epoch {} and iteration {} is {}".format(ii,jj,loss_itr))
 	        print("##########Loss for epoch {} is {}#########".format(ii,np.array(l).mean()))
 	    import IPython; IPython.embed()
