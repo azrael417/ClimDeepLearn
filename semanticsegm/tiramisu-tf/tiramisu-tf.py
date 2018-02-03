efa70836201c1915977451ca2f3ecc4a8be97d66
Thorsten Kurth <thorstenkurth@me.com>
2018-02-02 16:05:20 -0800

    debugging

--- semanticsegm/tiramisu-tf/tiramisu-tf.py
+++ semanticsegm/tiramisu-tf/tiramisu-tf.py
@@ -1,11 +1,21 @@
 import tensorflow as tf
 import tensorflow.contrib.keras as tfk
-import horovod.tensorflow as hvd
 import numpy as np
+
+#horovod, yes or no?
+horovod=True
+try:
+    import horovod.tensorflow as hvd
+except:
+    horovod = False
+
 
 #GLOBAL CONSTANTS
 image_height = 96
 image_width = 144
+comm_rank = 0
+comm_local_rank = 0
+comm_size = 1
 
 
 def conv(x, nf, sz, wd, stride=1): 
@@ -92,27 +102,26 @@ def create_tiramisu(nb_classes, img_input, nb_dense_block=6,
         skips,added = down_path(x, nb_layers, growth_rate, p, wd, training=training)
     
     with tf.name_scope("up_path") as scope:
-	x = up_path(added, reverse(skips[:-1]),reverse(nb_layers[:-1]), growth_rate, p, wd,training=training)
+        x = up_path(added, reverse(skips[:-1]),reverse(nb_layers[:-1]), growth_rate, p, wd,training=training)
 
     with tf.name_scope("conv_output") as scope:
-	x = conv(x,nb_classes,sz=1,wd=wd)
+        x = conv(x,nb_classes,sz=1,wd=wd)
         if p: x = tf.layers.dropout(x, rate=p, training=training)
         _,r,c,f = x.get_shape().as_list()
-	x = tf.reshape(x,[-1,image_height,image_width,nb_classes])
-	x = tf.nn.softmax(x)
-    return x
+    x = tf.reshape(x,[-1,image_height,image_width,nb_classes])
+    return x, tf.nn.softmax(x)
 
 
 #Load Data
 def load_data():
     #Load the images and the labels
-    imgs = np.load("/global/cscratch1/sd/tkurth/gb2018/tiramisu/small_set/images.npy").astype(np.float32)
+    imgs = np.load("/global/cscratch1/sd/tkurth/gb2018/tiramisu/tiny_set/images.npy").astype(np.float32)
     imgs = imgs.reshape([imgs.shape[0],imgs.shape[1],imgs.shape[2],1])
-    labels = np.load("/global/cscratch1/sd/tkurth/gb2018/tiramisu/small_set/masks.npy").astype(np.int32)
+    labels = np.load("/global/cscratch1/sd/tkurth/gb2018/tiramisu/tiny_set/masks.npy").astype(np.int32)
     
     #Image metadata contains year, month, day, time_step, and lat/ lon data for each crop.  
     #See README in $SCRATCH/segmentation_labels/dump_v4 on CORI
-    image_metadata = np.load("/global/cscratch1/sd/tkurth/gb2018/tiramisu/small_set/image_metadata.npy")
+    image_metadata = np.load("/global/cscratch1/sd/tkurth/gb2018/tiramisu/tiny_set/image_metadata.npy")
     
     #do some slicing
     imgs = imgs[:,3:-3,...]
@@ -157,13 +166,18 @@ def load_data():
 #main function
 def main():
     #init horovod
-    hvd.init()
+    if horovod:
+        hvd.init()
+        comm_rank = hvd.rank() 
+        comm_local_rank = hvd.local_rank()
+        comm_size = hvd.size()
+        print("Using distributed computation with Horovod: {} total ranks, I am rank {}".format(comm_size,comm_rank))
     
     #parameters
     batch = 32
     blocks = [3,3,4,7,10]
-    #num_epochs = 10
-    num_epochs = 2
+    num_epochs = 150
+    #num_epochs = 2
     
     #get data
     training_graph = tf.Graph()
@@ -182,13 +196,13 @@ def main():
         tst_lab_placeholder = tf.placeholder(tst_labels.dtype, tst_labels.shape, name="test-labels-placeholder")
         #train dataset
         trn_dataset = tf.data.Dataset.from_tensor_slices((trn_feat_placeholder, trn_lab_placeholder))
-        trn_dataset = trn_dataset.shard(hvd.size(),hvd.rank())
+        trn_dataset = trn_dataset.shard(comm_size, comm_rank)
         trn_dataset = trn_dataset.shuffle(buffer_size=100000)
         trn_dataset = trn_dataset.repeat(num_epochs)
         trn_dataset = trn_dataset.batch(batch)
         #validation dataset
         val_dataset = tf.data.Dataset.from_tensor_slices((val_feat_placeholder, val_lab_placeholder))
-        val_dataset = val_dataset.shard(hvd.size(),hvd.rank())
+        val_dataset = val_dataset.shard(comm_size, comm_rank)
         val_dataset = val_dataset.repeat(1)
         val_dataset = val_dataset.batch(batch)
         #test dataset
@@ -211,7 +225,7 @@ def main():
                                intra_op_parallelism_threads=33,
                                log_device_placement=False,
                                allow_soft_placement=True)
-    sess_config.gpu_options.visible_device_list = str(hvd.local_rank())
+    sess_config.gpu_options.visible_device_list = str(comm_local_rank)
 
 
     #create graph
@@ -219,42 +233,64 @@ def main():
         #set up model
         #images = tf.placeholder(tf.float32, [None, trn.shape[1], trn.shape[2], 1])
         #labels = tf.placeholder(tf.int32, [None, trn.shape[1], trn.shape[2], 1])
-        model = create_tiramisu(3, next_elem[0], nb_layers_per_block=blocks, p=0.2, wd=1e-4)
-        loss = tf.losses.sparse_softmax_cross_entropy(labels=next_elem[1],logits=model)
+        logit, prediction = create_tiramisu(3, next_elem[0], nb_layers_per_block=blocks, p=0.2, wd=1e-4)
+        loss = tf.losses.sparse_softmax_cross_entropy(labels=next_elem[1],logits=logit)
         global_step = tf.train.get_or_create_global_step()
         #set up optimizer
         opt = tf.train.RMSPropOptimizer(learning_rate=1e-3)
-        opt = hvd.DistributedOptimizer(opt)
+        if horovod:
+            opt = hvd.DistributedOptimizer(opt)
         train_op = opt.minimize(loss, global_step=global_step)
         #set up streaming metrics
         labels_one_hot = tf.contrib.layers.one_hot_encoding(next_elem[1], 3)
-        iou_op, iou_update_op = tf.metrics.mean_iou(model,labels_one_hot,3,weights=None,metrics_collections=None,updates_collections=None,name="iou_score")
+        iou_op, iou_update_op = tf.metrics.mean_iou(prediction,labels_one_hot,3,weights=None,metrics_collections=None,updates_collections=None,name="iou_score")
         
         #compute epochs and stuff:
-        num_samples = trn.shape[0] // hvd.size()
+        num_samples = trn.shape[0] // comm_size
         num_steps_per_epoch = num_samples // batch
         num_steps = num_epochs*num_steps_per_epoch
         
         #hooks
         #these hooks are essential. regularize the step hook by adding one additional step at the end
-        hooks = [hvd.BroadcastGlobalVariablesHook(0), tf.train.StopAtStepHook(last_step=num_steps+1)]
+        hooks = [tf.train.StopAtStepHook(last_step=num_steps+1)]
+        if horovod:
+            hooks.append(hvd.BroadcastGlobalVariablesHook(0))
         #initializers:
         init_op =  tf.global_variables_initializer()
         init_local_op = tf.local_variables_initializer()
         
         #checkpointing
-        if hvd.rank() == 0:
-            checkpoint_dir = './checkpoints' if hvd.rank() == 0 else None
+        if comm_rank == 0:
+            checkpoint_dir = './checkpoints'
             checkpoint_save_freq = num_steps_per_epoch
             checkpoint_saver = tf.train.Saver(max_to_keep = 1000)
             hooks.append(tf.train.CheckpointSaverHook(checkpoint_dir=checkpoint_dir, save_steps=checkpoint_save_freq, saver=checkpoint_saver))
         
+        #DEBUG
+        #summary
+        if comm_rank == 0:
+            print("write graph for debugging")
+            tf.summary.scalar("loss",loss)
+            summary_op = tf.summary.merge_all()
+            #hooks.append(tf.train.SummarySaverHook(save_steps=num_steps_per_epoch, summary_writer=summary_writer, summary_op=summary_op))
+            with tf.Session(config=sess_config) as sess:
+                sess.run([init_op, init_local_op])
+                #create iterator handles
+                trn_handle, val_handle = sess.run([trn_iterator.string_handle(), val_iterator.string_handle()])
+                #init iterators
+                sess.run(trn_init_op, feed_dict={handle: trn_handle, trn_feat_placeholder: trn, trn_lab_placeholder: trn_labels})
+                #summary:
+                sess.run(summary_op, feed_dict={handle: trn_handle})
+                #summary file writer
+                summary_writer = tf.summary.FileWriter('./logs', sess.graph)
+        #DEBUG
+        
+
         #start session
         with tf.train.MonitoredTrainingSession(config=sess_config, hooks=hooks) as sess:
             #initialize
             sess.run([init_op, init_local_op])
-            #iterators
-            #create handles
+            #create iterator handles
             trn_handle, val_handle = sess.run([trn_iterator.string_handle(), val_iterator.string_handle()])
             #init iterators
             sess.run(trn_init_op, feed_dict={handle: trn_handle, trn_feat_placeholder: trn, trn_lab_placeholder: trn_labels})
@@ -274,13 +310,13 @@ def main():
                     
                     if train_steps_in_epoch > 0:
                         #print step report
-                        print("REPORT: rank {}, training loss for step {} (of {}) is {}".format(hvd.rank(), train_steps, num_steps, train_loss/train_steps_in_epoch))
+                        print("REPORT: rank {}, training loss for step {} (of {}) is {}".format(comm_rank, train_steps, num_steps, train_loss/train_steps_in_epoch))
                     else:
                         #print epoch report
                         train_loss /= num_steps_per_epoch
-                        print("COMPLETED: rank {}, training loss for epoch {} (of {}) is {}".format(hvd.rank(), epoch, num_epochs, train_loss))
+                        print("COMPLETED: rank {}, training loss for epoch {} (of {}) is {}".format(comm_rank, epoch, num_epochs, train_loss))
                         iou_score = sess.run(iou_op)
-                        print("COMPLETED: rank {}, training IoU for epoch {} (of {}) is {}".format(hvd.rank(), epoch, num_epochs, iou_score))
+                        print("COMPLETED: rank {}, training IoU for epoch {} (of {}) is {}".format(comm_rank, epoch, num_epochs, iou_score))
                         
                         #evaluation loop
                         eval_loss = 0.
@@ -293,9 +329,9 @@ def main():
                                 eval_steps += 1
                             except tf.errors.OutOfRangeError:
                                 eval_loss /= eval_steps
-                                print("COMPLETED: rank {}, evaluation loss for epoch {} (of {}) is {}".format(hvd.rank(), epoch-1, num_epochs, eval_loss))
+                                print("COMPLETED: rank {}, evaluation loss for epoch {} (of {}) is {}".format(comm_rank, epoch-1, num_epochs, eval_loss))
                                 iou_score = sess.run(iou_op)
-                                print("COMPLETED: rank {}, evaluation IoU for epoch {} (of {}) is {}".format(hvd.rank(), epoch-1, num_epochs, iou_score))
+                                print("COMPLETED: rank {}, evaluation IoU for epoch {} (of {}) is {}".format(comm_rank, epoch-1, num_epochs, iou_score))
                                 sess.run(val_init_op, feed_dict={handle: val_handle, val_feat_placeholder: val, val_lab_placeholder: val_labels})
                                 break
                                 
