079f85291d614654d4fc7d9480c0f6ee9d86e5c1
Thorsten Kurth <thorstenkurth@me.com>
2018-03-09 09:03:21 -0500

    helpers

--- semanticsegm/run_scripts/run_summit.sh
+++ semanticsegm/run_scripts/run_summit.sh
@@ -26,6 +26,7 @@ run_dir=${SWORK}/tuning/run_nn${nnodes}_np${nprocs}_j${LSB_JOBID}
 mkdir -p ${run_dir}
 cp stage_in.sh ${run_dir}/
 cp ../tiramisu-tf/mascarpone-tiramisu-tf*.py ${run_dir}/
+cp ../tiramisu-tf/tiramisu-helpers.py ${run_dir}/
 
 #step in
 cd ${run_dir}
@@ -36,5 +37,5 @@ scratchdir="/xfs/scratch/"$(whoami)"/data"
 
 #run
 cat ${LSB_DJOB_HOSTFILE} | sort | uniq | grep -v login | grep -v batch > host_list
-mpirun -np ${nnodes} --bind-to none -x PATH -x LD_LIBRARY_PATH --hostfile host_list -npernode 1 ./stage_in.sh ${datadir} ${scratchdir} 1000
-mpirun -np ${nprocs} --bind-to none -x PATH -x LD_LIBRARY_PATH --hostfile host_list -npernode ${nprocspn} python ./mascarpone-tiramisu-tf-singlefile.py --blocks 3 3 4 4 7 7 10 --loss weighted --lr 1e-5 --datadir ${scratchdir} |& tee out.${LSB_JOBID}
+#mpirun -np ${nnodes} --bind-to none -x PATH -x LD_LIBRARY_PATH --hostfile host_list -npernode 1 ./stage_in.sh ${datadir} ${scratchdir} 1000
+mpirun -np ${nprocs} --bind-to none -x PATH -x LD_LIBRARY_PATH --hostfile host_list -npernode ${nprocspn} python ./mascarpone-tiramisu-tf-singlefile.py --blocks 3 3 4 4 7 7 10 --loss weighted --lr 1e-5 --datadir ${datadir} |& tee out.${LSB_JOBID}
