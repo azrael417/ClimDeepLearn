944c8ed6fc52dc583d890568cad84db72ad71bf8
Josh Romero <romero.josh@gmail.com>
2018-03-25 15:18:31 -0400

    Removing extraneous summit run scripts.

--- semanticsegm/run_scripts/stage_in_2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#arguments: 
-#$1 - source dir
-#$2 - destination dir
-#$3 - maximum files to stage (total, not per node). Set to -1 if you want to stage everything
-
-python ./stagein.py --prefix="data" --input_path=${1} --output_path=${2} --max_files=${3} --clean
-cp ${1}/stats.h5 ${2}/
