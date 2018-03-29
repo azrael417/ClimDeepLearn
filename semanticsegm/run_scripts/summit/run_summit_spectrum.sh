05e9b6557922442878c9d04303fc3b2d45aae1fc
Josh Romero <romero.josh@gmail.com>
2018-03-29 19:17:27 -0400

    Updating summit run scripts to use packaged python environment.

--- semanticsegm/run_scripts/summit/run_summit_spectrum.sh
+++ semanticsegm/run_scripts/summit/run_summit_spectrum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#BSUB -nnodes 1
-##BSUB -csm y
-##BSUB -R "1*{select[LN]span[hosts=1]} + 43008*{select[CN &&  (hname !='c35n15') && (hname !='d04n05')]order[!-slots:maxslots]span[ptile=42] }"
+##BSUB -nnodes 4
+#BSUB -csm y
+#BSUB -R "1*{select[LN]span[hosts=1]} + 168*{select[CN &&  (hname !='c35n15') && (hname !='d04n05') && (hname != 'b25n01') && (hname != 'b25n02') && (hname != 'b25n03') && (hname != 'b25n04') && (hname != 'b25n05') && (hname != 'b25n06') && (hname != 'b25n07') && (hname != 'b25n08') && (hname != 'b25n09') && (hname != 'b25n10') && (hname != 'b25n11') && (hname != 'b25n12') && (hname != 'b25n13') && (hname != 'b25n14') && (hname != 'b25n15') && (hname != 'b25n16')]order[!-slots:maxslots]span[ptile=42] }"
 #BSUB -W 60
 #BSUB -P VEN101SUMMIT
 #BSUB -alloc_flags "smt4 nvme"
@@ -10,15 +10,16 @@
 #BSUB -e out_test.%J
 #BSUB -q batch
 
+# load modules
+module load cuda
+
 #determine number of nodes and total procs
 nprocspn=6
 nnodes=$(cat ${LSB_DJOB_HOSTFILE} | sort | uniq | grep -v login | grep -v batch | wc -l)
 nprocs=$(( ${nnodes} * ${nprocspn} ))
 
-
 #script in place
 SWORK=/gpfs/alpinetds/scratch/mfatica/ven101
-#run_dir=${SWORK}/SC_scaling_r2/run_nn${nnodes}_np${nprocs}_j${LSB_JOBID}
 run_dir=${SWORK}/testing/run_nn${nnodes}_np${nprocs}_j${LSB_JOBID}
 mkdir -p ${run_dir}
 
@@ -34,18 +35,18 @@ cd ${run_dir}
 
 #datadir
 datadir="/gpfs/alpinetds/world-shared/csc275/climdata_reformat"
-scratchdir="/xfs/scratch/"$(whoami)"/data"
+scratchdir="/xfs/scratch/"$(whoami)""
 nperproc=250
 numfiles=$(( ${nprocspn} * ${nperproc} )) 
 
 #run
 cat ${LSB_DJOB_HOSTFILE} | sort | uniq | grep -v login | grep -v batch > host_list
-#jsrun -n ${nnodes} -g 1 -c 42 -a 1 ./stage_in_2_spectrum.sh ${datadir} ${scratchdir} ${numfiles}
 
 echo "starting stage_in_parallel.sh"
 jsrun -n ${nnodes} -g 1 -c 42 -a 1 ./stage_in_parallel.sh ${datadir} ${scratchdir} ${numfiles}
-echo "finished stage_in.sh"
+echo "finished stage_in_parallel.sh"
+
 echo "starting run_mascarpone.sh"
-jsrun -n ${nnodes} -g 6 -c 42 -a ${nprocspn} --bind=proportional-packed:7 --launch_distribution=packed stdbuf -o0 ./run_mascarpone.sh |& tee out.fp32.${LSB_JOBID}
-jsrun -n ${nnodes} -g 6 -c 42 -a ${nprocspn} --bind=proportional-packed:7 --launch_distribution=packed stdbuf -o0 ./run_mascarpone_fp16.sh |& tee out.fp16.${LSB_JOBID}
+jsrun -n ${nnodes} -g 6 -c 42 -a ${nprocspn} --bind=proportional-packed:7 --launch_distribution=packed stdbuf -o0 ./run_mascarpone.sh ${scratchdir} |& tee out.fp32.${LSB_JOBID}
+jsrun -n ${nnodes} -g 6 -c 42 -a ${nprocspn} --bind=proportional-packed:7 --launch_distribution=packed stdbuf -o0 ./run_mascarpone_fp16.sh ${scratchdir} |& tee out.fp16.${LSB_JOBID}
 echo "finished run_mascarpone.sh"
