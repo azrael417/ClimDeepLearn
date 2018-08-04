90ea253ee7d05c26f085fa27c6343c8d7c40099e
Thorsten Kurth <thorstenkurth@me.com>
2018-08-04 17:17:54 -0400

    adding run_env to git

--- semanticsegm/run_scripts/bigruns_update/run_env.sh
+++ semanticsegm/run_scripts/bigruns_update/run_env.sh
@@ -4,3 +4,13 @@
 module load xl
 module load cuda/9.2.88
 module load essl
+
+#set core files to 0 size
+ulimit -c 0
+
+# Disable multiple threads
+export OMPI_MCA_osc_pami_allow_thread_multiple=0
+
+#disable adaptive routing
+export PAMI_IBV_ENABLE_OOO_AR=0
+export PAMI_IBV_QP_SERVICE_LEVEL=0
