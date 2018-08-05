5dfd8d9d98da8b5b3ac89647d61c0bb66b4c5911
Thorsten Kurth <thorstenkurth@me.com>
2018-08-05 18:55:06 -0400

    fixed some printing issues

--- semanticsegm/run_scripts/bigruns_update/run_env.sh
+++ semanticsegm/run_scripts/bigruns_update/run_env.sh
@@ -5,11 +5,18 @@ module load xl
 module load cuda/9.2.88
 module load essl
 
+#load old spectrum
+module load spectrum-mpi/10.2.0.4-20180716
+
 #set core files to 0 size
 ulimit -c 0
 
 # Disable multiple threads
 export OMPI_MCA_osc_pami_allow_thread_multiple=0
+
+#enable adaptive routing
+#export PAMI_IBV_ENABLE_OOO_AR=1
+#export PAMI_IBV_QP_SERVICE_LEVEL=8
 
 #disable adaptive routing
 export PAMI_IBV_ENABLE_OOO_AR=0
