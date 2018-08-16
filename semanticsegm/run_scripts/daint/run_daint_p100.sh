9db52ed8fdfa003699d8f2a91e88fba16dad7d9c
Thorsten Kurth <tkurth@lbl.gov>
2018-08-16 22:41:59 +0200

    updated daint scripts

--- semanticsegm/run_scripts/daint/run_daint_p100.sh
+++ semanticsegm/run_scripts/daint/run_daint_p100.sh
@@ -12,9 +12,7 @@ module unload PrgEnv-cray
 module load PrgEnv-gnu
 module load gcc/5.3.0
 module load cudatoolkit/8.0.61_2.4.3-6.0.4.0_3.1__gb475d12
-module load cray-hdf5/1.10.0.3
-module load craype-hugepages2M
-source activate tensorflow-hp
+source activate tensorflow-py27
 
 #openmp stuff
 export OMP_NUM_THREADS=12
@@ -24,22 +22,28 @@ export CRAY_CUDA_MPS=0
 export MPICH_RDMA_ENABLED_CUDA=1
 
 #directories
-datadir=/scratch/snx3000/tkurth/data/tiramisu/segm_h5_v3_reformat
-scratchdir=/dev/shm/$(whoami)/tiramisu
-numfiles=500
+datadir=/scratch/snx3000/tkurth/data/segm_h5_v3_new_split
+scratchdir=/dev/shm/$(whoami)
+numfiles_train=200
+numfiles_validation=20
 
 #create run dir
-rundir=${WORK}/data/tiramisu/runs/scaling_2018-03-25/run_nnodes${SLURM_NNODES}_j${SLURM_JOBID}
-#rundir=${WORK}/data/tiramisu/runs/run_nnodes16_j6415751
-mkdir -p ${rundir}
-cp ../../tiramisu-tf/tiramisu_helpers.py ${rundir}/
-cp ../../tiramisu-tf/mascarpone-tiramisu-tf-singlefile.py ${rundir}/
-cp ./parallel_stagein.sh ${rundir}/
-cp ../../tiramisu-tf/parallel_stagein.py ${rundir}/
+run_dir=${WORK}/data/deeplab/runs/run_nnodes${SLURM_NNODES}_j${SLURM_JOBID}
+mkdir -p ${run_dir}
+
+#copy relevant files
+cp stage_in_parallel.sh ${run_dir}/
+cp ../../utils/parallel_stagein.py ${run_dir}/
+cp ../../utils/graph_flops.py ${run_dir}/
+cp ../../utils/climseg_helpers.py ${run_dir}/
+cp ../../deeplab-tf/deeplab-tf.py ${run_dir}/
 
 #step in
-cd ${rundir}
+cd ${run_dir}
 
 #run the training
-srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 24 ./parallel_stagein.sh ${datadir} ${scratchdir} ${numfiles}
-srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 24 -u python -u ./mascarpone-tiramisu-tf-singlefile.py --fs local --datadir ${scratchdir} --blocks 2 2 2 4 5 --growth 32 --cluster_loss_weight 0.01 --filter-sz 5 --lr 1e-4 --optimizer="LARC-Adam" |& tee out.${SLURM_JOBID}
+#stage in
+srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 24 ./stage_in_parallel.sh ${datadir} ${scratchdir} ${numfiles_train} ${numfiles_validation}
+
+#fp32 lag 1
+srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 24 -u python ./deeplab-tf.py --datadir_train ${scratchdir}/train/data --datadir_validation ${scratchdir}/validation/data --chkpt_dir checkpoint.fp32.lag1 --epochs 4 --fs local --loss weighted --cluster_loss_weight 0.0 --optimizer opt_type=LARC-Adam,learning_rate=0.001,gradient_lag=0 --model=resnet_v2_50 --scale_factor 0.1 --batch 1 --decoder=deconv1x |& tee out.fp32.lag1.${SLURM_JOBID}
