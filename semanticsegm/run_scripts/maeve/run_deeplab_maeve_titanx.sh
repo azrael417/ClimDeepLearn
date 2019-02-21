508fc89107a603b781f6a457cb30b367ea9ba40d
Thorsten Kurth <thorstenkurth@me.com>
2019-02-21 09:45:35 -0800

    committed

--- semanticsegm/run_scripts/maeve/run_deeplab_maeve_titanx.sh
+++ semanticsegm/run_scripts/maeve/run_deeplab_maeve_titanx.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#load python env
-source activate thorsten-tf-py27
-
 #openmp stuff
 export OMP_NUM_THREADS=6
 export OMP_PLACES=threads
@@ -12,17 +9,18 @@ export OMP_PROC_BIND=spread
 export CUDA_VISIBLE_DEVICES=2
 
 #directories
+datadir=/mnt/data
+scratchdir=/mnt/data
 #datadir=/data1/tkurth/tiramisu/segm_h5_v3_new_split
-datadir=/data1/mudigonda/missing_files_for_gb_video
+#datadir=/data1/mudigonda/missing_files_for_gb_video
 #scratchdir=${DW_PERSISTENT_STRIPED_DeepCAM}/$(whoami)
-scratchdir=${datadir}
 numfiles_train=1500
 numfiles_validation=300
 numfiles_test=500
 
 #create run dir
 #run_dir=/data1/tkurth/deeplab/runs/run_1
-run_dir=/data1/tkurth/deeplab/runs/test_run_nn256_np1536_j143026_convergence
+run_dir=/mnt/runs/deeplab/run1_ngpus1
 
 mkdir -p ${run_dir}
 
@@ -39,37 +37,49 @@ cp ../../deeplab-tf/deeplab_model.py ${run_dir}/
 #step in
 cd ${run_dir}
 
-
 #some parameters
+stage=1
 lag=0
-train=0
-test=1
+train=1
+test=0
 
+#stage in
+if [ ${stage} -eq 1 ]; then
+    if [ "${datadir}" != "${scratchdir}" ]; then
+	cmd="./stage_in_parallel.sh ${datadir} ${scratchdir} ${numfiles_train} ${numfiles_validation} ${numfiles_test}"
+	echo ${cmd}
+	${cmd}
+    else
+	echo "Staging not necessary, data directory and scratch directory are the same"
+    fi
+fi
+
+#train
 if [ ${train} -eq 1 ]; then
   echo "Starting Training"
   python -u ./deeplab-tf-train.py      --datadir_train ${scratchdir}/train \
                                        --train_size ${numfiles_train} \
                                        --datadir_validation ${scratchdir}/validation \
                                        --validation_size ${numfiles_validation} \
-                                       --chkpt_dir checkpoint.fp16.lag${lag} \
+                                       --chkpt_dir checkpoint.fp32.lag${lag} \
                                        --epochs 20 \
                                        --fs local \
                                        --loss weighted_mean \
                                        --optimizer opt_type=LARC-Adam,learning_rate=0.0001,gradient_lag=${lag} \
                                        --model=resnet_v2_50 \
                                        --scale_factor 1.0 \
-                                       --batch 2 \
+                                       --batch 1 \
                                        --decoder deconv1x \
                                        --device "/device:cpu:0" \
-                                       --dtype float16 \
+                                       --dtype float32 \
 				       --label_id 0 \
-                                       --data_format "channels_last" |& tee out.fp16.lag${lag}.train
+                                       --data_format "channels_last" |& tee out.fp32.lag${lag}.train
 fi
 
 if [ ${test} -eq 1 ]; then
   echo "Starting Testing"
   python -u ./deeplab-tf-inference.py      --datadir_test ${scratchdir}/test \
-                                           --chkpt_dir checkpoint.fp16.lag${lag} \
+                                           --chkpt_dir checkpoint.fp32.lag${lag} \
 					   --test_size -1 \
 					   --output_graph deepcam_inference.pb \
                                            --output output_test_5 \
@@ -77,10 +87,10 @@ if [ ${test} -eq 1 ]; then
                                            --loss weighted_mean \
                                            --model=resnet_v2_50 \
                                            --scale_factor 1.0 \
-                                           --batch 2 \
+                                           --batch 1 \
                                            --decoder deconv1x \
                                            --device "/device:cpu:0" \
-                                           --dtype float16 \
+                                           --dtype float32 \
 					   --label_id 0 \
-                                           --data_format "channels_last" |& tee out.fp16.lag${lag}.test
+                                           --data_format "channels_last" |& tee out.fp32.lag${lag}.test
 fi
